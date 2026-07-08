d4f0cb91982bed1385b55d094a28fd12fd478de6
impressivecoder1 <rohanrahman251@gmail.com>
2026-07-09 01:19:01 +0600

    insert data for user

--- assignment.sql
+++ assignment.sql
@@ -5,3 +5,10 @@ CREATE TABLE Users (
     role VARCHAR(50) NOT NULL,
     phone_number VARCHAR(20)
 );
+INSERT INTO Users
+(user_id, full_name, email, role, phone_number)
+VALUES
+(1, 'Tanvir Rahman', 'tanvir@mail.com', 'Football Fan', '+8801711111111'),
+(2, 'Asif Haque', 'asif@mail.com', 'Football Fan', '+8801722222222'),
+(3, 'Sajjad Rahman', 'sajjad@mail.com', 'Ticket Manager', '+8801733333333'),
+(4, 'Jannat Ara', 'jannat@mail.com', 'Football Fan', NULL);
