0554c1ceb6547d694adef12c425e1ec1b1d2ec21
impressivecoder1 <rohanrahman251@gmail.com>
2026-07-09 01:37:59 +0600

    second query done

--- assignment.sql
+++ assignment.sql
@@ -56,3 +56,8 @@ SELECT
 FROM Matches
 WHERE tournament_category = 'Champions League'
 AND match_status = 'Available';
+--2
+SELECT user_id, full_name, email
+FROM Users
+WHERE full_name ILIKE 'Tanvir%'
+OR full_name ILIKE '%Haque%';
