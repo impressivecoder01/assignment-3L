ad9ac5aad47231e8e51173c80cc797f7e74b606a
impressivecoder1 <rohanrahman251@gmail.com>
2026-07-09 01:17:47 +0600

    first commit

---
+++ assignment.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Users (
+    user_id INT PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    role VARCHAR(50) NOT NULL,
+    phone_number VARCHAR(20)
+);
