1b39a1e2d4c5c1a08debe44c29dbbd938d3c5673
impressivecoder1 <rohanrahman251@gmail.com>
2026-07-09 01:59:49 +0600

    sever

--- assignment.sql
+++ assignment.sql
@@ -90,3 +90,13 @@ FROM
     Bookings
 WHERE 
     total_cost > (SELECT AVG(total_cost) FROM Bookings);
+    --7
+SELECT 
+    match_id, 
+    fixture, 
+    base_ticket_price
+FROM 
+    Matches
+ORDER BY 
+    base_ticket_price DESC
+LIMIT 2 OFFSET 1;
