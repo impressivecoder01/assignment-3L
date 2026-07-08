09b87b8ea70c433baa0d04726e24ec38602195f0
impressivecoder1 <rohanrahman251@gmail.com>
2026-07-09 01:29:09 +0600

    booking table done

--- assignment.sql
+++ assignment.sql
@@ -12,6 +12,7 @@ VALUES
 (2, 'Asif Haque', 'asif@mail.com', 'Football Fan', '+8801722222222'),
 (3, 'Sajjad Rahman', 'sajjad@mail.com', 'Ticket Manager', '+8801733333333'),
 (4, 'Jannat Ara', 'jannat@mail.com', 'Football Fan', NULL);
+--match table 
 CREATE TABLE Matches (
     match_id INT PRIMARY KEY,
     fixture VARCHAR(100) NOT NULL,
@@ -27,3 +28,22 @@ VALUES
 (103, 'Bayern Munich vs PSG', 'Champions League', 130, 'Available'),
 (104, 'AC Milan vs Inter Milan', 'Serie A', 90, 'Sold Out'),
 (105, 'Juventus vs Roma', 'Serie A', 80, 'Available');
+
+--bookings
+CREATE TABLE Bookings (
+    booking_id INT PRIMARY KEY,
+    user_id INT REFERENCES Users(user_id),
+    match_id INT REFERENCES Matches(match_id),
+    seat_number VARCHAR(10),
+    payment_status VARCHAR(20),
+    total_cost DECIMAL(10,2)
+);
+
+INSERT INTO Bookings
+(booking_id, user_id, match_id, seat_number, payment_status, total_cost)
+VALUES
+(501, 1, 101, 'A-12', 'Confirmed', 150),
+(502, 1, 102, 'B-04', 'Confirmed', 120),
+(503, 2, 101, 'A-13', 'Confirmed', 150),
+(504, 2, 101, NULL, NULL, 150),
+(505, 3, 102, 'C-20', 'Pending', 120);
